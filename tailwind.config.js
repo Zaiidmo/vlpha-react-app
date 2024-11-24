8800cab8db10afc81fd558369efd7b4328b1ab40
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-11-24 11:07:52 +0100

    Applying the poppins fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -91,7 +91,10 @@ export default {
   			'accordion-down': 'accordion-down 0.2s ease-out',
   			'accordion-up': 'accordion-up 0.2s ease-out',
 			'logo-spin': 'logo-spin 2s linear infinite'
-  		}
+  		},
+		fontFamily: {
+			"poppins": "Poppins",
+		}
   	}
   },
   plugins: [require("tailwindcss-animate")],
