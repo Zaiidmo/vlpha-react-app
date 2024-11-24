ad576eae43dbd0ff6f384fa6c2cd1c2d4fd4755d
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-11-24 13:47:57 +0100

    Create AxiosInstance, Add Utils for Toast Notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, useRoutes } from 'react-router-dom'
 import Layout from './layouts/clientLayout'
 import routes from './routes/routes'
+import { Toaster } from 'react-hot-toast';
 
 function AppRoutes() {
   const element = useRoutes(routes)
@@ -11,6 +12,17 @@ function App() {
   return (
     <Router>
       <Layout>
+      <Toaster
+        position="bottom-right"
+        reverseOrder={false}
+        toastOptions={{
+          duration: 4000,
+          style: {
+            background: '#333',
+            color: '#fff',
+          },
+        }}
+      />
         <AppRoutes />
       </Layout>
     </Router>
---
+++ src/services/api/axiosInstance.ts
@@ -0,0 +1,49 @@
+import { logout } from "@/store/slices/authSlice";
+import { RootState, store } from "@/store/store";
+import { toastNotifier } from "@/utils/toastNotifier";
+import axios from "axios";
+import { useSelector } from "react-redux";
+
+//Axios Instance
+const axiosInstance = axios.create({
+  baseURL: process.env.API_URL,
+  timeout: 3000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+//Request Interceptor
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = useSelector((state: RootState) => state.auth.token);
+    if (token) {
+      config.headers.Authorization == `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response Interceptor
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if( error.response?.status === 400) {
+            toastNotifier({
+                message: 'Unauthorized. Redirecting to login ...',
+                type: "error",
+                duration: 3000
+            })
+            store.dispatch(logout());
+
+            setTimeout(() => {
+                window.location.href = '/login';
+            }, 3000);
+            
+        }
+        return Promise.reject(error)
+    }
+)
